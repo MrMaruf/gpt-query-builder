0d09b0e4769aa845d9b276aa9882191ca0b1c4a0
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-11-19 09:17:08 +0100

    feat: setting up some foundation for query storing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '@/views/AboutView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import QueryView from '@/views/query/QueryView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +9,24 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/about',
       name: 'about',
-      component: AboutView
-    }
-  ]
-})
+      component: AboutView,
+    },
+    {
+      path: '/query',
+      name: 'query',
+      component: QueryView,
+    },
+    {
+      path: '/query/:queryName',
+      name: 'query builder',
+      component: QueryView,
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/query/QueryBuilderView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import QueryMenu from '@/components/query/QueryMenu.vue';
+
+</script>
+
+<template>
+    <QueryMenu/>
+</template>
