8a931bf7c855a996d791612a3de5c1d76dd92c14
MrMaruf <abdirimov.maruf@gmail.com>
2023-12-19 18:48:52 +0100

    feat: added basic template, not tested

---
+++ src/views/SignInView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+let password: string = '';
+let email: string = '';
+
+const onSubmitForm = (event: Event) => {
+  event.preventDefault();
+  const formElement = event.currentTarget as HTMLFormElement;
+  const form = new FormData(formElement);
+  console.log(form);
+};
+</script>
+
+<template>
+  <form @submit="onSubmitForm">
+    <div class="mb-3">
+      <input type="text" name="email" v-model="email" />
+    </div>
+    <div class="mb-3">
+      <input type="text" name="password" v-model="password" />
+    </div>
+    <button>Sign in</button>
+  </form>
+</template>
+<style scoped></style>
